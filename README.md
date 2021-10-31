52b4de193dfe568b0bbc614f33e07123bee6fe00
Nigel Chiwaya <nchiwaya@gmail.com>
2021-10-30 23:35:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Open iframe in new tab
+
+Right click an iframe and open it in a new tab.
+
+How to deploy a new version: https://developer.chrome.com/webstore/publish
+Extension dashboard: https://chrome.google.com/webstore/developer/dashboard
---
+++ background.js
@@ -0,0 +1,29 @@
+// /**                                                                                
+//  * Returns a handler which will open a new window when activated.                  
+//  */                                                                                
+function getClickHandler(destination){
+    if ( destination == 'localhost' ) {
+        return function() {
+            window.open('http://localhost:3000/', '_blank');
+        };
+    }
+
+    return function(info, tab){                                                        
+        const url = info.frameUrl
+        var path_array = url.split('/')
+        path_array.pop()
+        var dest_url = path_array.join('/') 
+        window.open(dest_url, '_blank');
+    };                                                                               
+};                                                                                 
+/**                                                                                
+ * Create a context menu which will only show up for URLs and images.
+ */                                                                                
+chrome.contextMenus.create({
+  "title": "Open this iframe",
+  "type": "normal",
+  "contexts": ["frame"],
+  "onclick": getClickHandler("text")
+});
+
+
