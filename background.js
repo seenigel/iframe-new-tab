a411cb4787ee848205ec5fcbd76071d5eca1a823
Nigel Chiwaya <nchiwaya@gmail.com>
2022-06-26 23:17:27 -0400

    updated to manifest v3

--- background.js
+++ background.js
@@ -1,26 +1,15 @@
-// /**                                                                                
-//  * Returns a handler which will open a new window when activated.                  
-//  */                                                                                
-function getClickHandler(destination){
-    if ( destination == 'localhost' ) {
-        return function() {
-            window.open('http://localhost:3000/', '_blank');
-        };
-    }
-
-    return function(info, tab){                                                        
-        const url = info.frameUrl
-        window.open(url, '_blank');
-    };                                                                               
-};                                                                                 
-/**                                                                                
- * Create a context menu which will only show up for URLs and images.
- */                                                                                
-chrome.contextMenus.create({
-  "title": "Open this iframe",
-  "type": "normal",
-  "contexts": ["frame"],
-  "onclick": getClickHandler("text")
+chrome.runtime.onInstalled.addListener(() => {                                                                  
+    chrome.contextMenus.create({
+        "id": 'iframe-getter',
+        "title": "Open this iframe",
+        "contexts": ["frame"],
+    });
 });
+chrome.contextMenus.onClicked.addListener((OnClickData)=>{
+    console.log('yoohoo')
+    let url = OnClickData.frameUrl
+    chrome.tabs.create({ "url": url });
+})
+
 
 
